84e98e3f14bf5ae77a9ba9073723e86900e27ba5
José Santos <josecorreia199@gmail.com>
2022-02-04 21:27:26 -0300

    Meus primeiros códigos em Pythom

---
+++ TestesPython.py
@@ -0,0 +1,9 @@
+nome = input('Qual é o seu nome')
+idade = input('Qual é a sua idade')
+
+print('Olá', nome, 'você tem', idade, 'anos. É um prazer te conhecer')
+
+numero = input('Digite um número')
+soma = int(idade)+int(numero)
+print('Sua idade é ', soma)
+
